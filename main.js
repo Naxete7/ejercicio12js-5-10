cf28482da3e36a0b891905f9942988ec128c16ab
Ignacio <naxete7@hotmail.es>
2022-10-05 23:26:58 +0200

    ejercicio 12 casi acabado

---
+++ main.js
@@ -0,0 +1,20 @@
+// Escribe una aplicación con un String que contenga una contraseña cualquiera.
+// Después se te pedirá que introduzcas la contraseña, con 3 intentos. Cuando
+// aciertes ya no pedirá más la contraseña y mostrará un mensaje diciendo
+// “Enhorabuena”. Piensa bien en la condición de salida (3 intentos y si acierta sale,
+// aunque le queden intentos).
+
+
+let contraseña = ("Olivia")
+
+let usuario = prompt("Escriba aqui su contraseña")
+
+for (i = 0; i <= 3; i++) {
+
+    if (usuario === contraseña) {
+        console.log("Su contraseña es correcta")
+    } else {
+        console.log("Su contraseña es incorrecta")
+    }
+
+};
